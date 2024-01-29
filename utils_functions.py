401603f3779ca5d5ee0df4ff16699065e13bedf3
AleG19 <monteleone.1883922@studenti.uniroma1.it>
2024-01-29 12:54:46 +0100

    added field building in graph form

--- utils_functions.py
+++ utils_functions.py
@@ -1,23 +1,58 @@
 from random import randint
+from torch_geometric.data import Data
+import torch
 
 
-class Cell:
-    def __init__(self, val: int, wormhole: bool):
-        self.val = val
-        self.wormhole = wormhole
-
-
-def apply_wormholes_policy(row: list[str], enable_wormholes: bool = False, max_val: int = 0):
-    out_row = []
-    for el in row:
-        if el == "*":
-            if enable_wormholes:
-                cell = Cell(0, True)
-            elif not enable_wormholes and max_val > 0:
-                cell = Cell(randint(0, max_val), False)
+def build_field(table: list[list[str]], enable_wormholes: bool = False, max_val: int = 0):
+    edge_begin = []
+    edge_end = []
+    nodes = []
+    len_row = len(table[0])
+    if enable_wormholes:
+        near_wormholes = build_near_wormholes(table)
+    for i in range(len(table)):
+        for j in range(len_row):
+            if table[i][j] == "*":
+                if enable_wormholes:
+                    pass
+                else:
+                    node = randint(0,max_val)
             else:
-                cell = Cell(0, False)
-        else:
-            cell = Cell(int(el), False)
-        out_row.append(cell)
-    return out_row
+                node = int(table[i][j])
+            nodes.append(node)
+            if i + 1 < len(table):
+                edge_begin.append(i * len_row + j)
+                edge_end.append((i+1) * len_row + j)
+            if i - 1 >= 0:
+                edge_begin.append(i * len_row + j)
+                edge_end.append((i - 1) * len_row + j)
+            if j + 1 < len_row:
+                edge_begin.append(i * len_row + j)
+                edge_end.append(i * len_row + j+1)
+            if j - 1 >= 0:
+                edge_begin.append(i * len_row + j)
+                edge_end.append(i * len_row + j-1)
+    edge_begin = torch.tensor(edge_begin, dtype=torch.int)
+    edge_end = torch.tensor(edge_end, dtype=torch.int)
+    nodes = torch.tensor(nodes, dtype=torch.int)
+    edge_index = torch.cat([edge_begin.unsqueeze(0), edge_end.unsqueeze(0)], dim=0)
+    return Data(x=nodes, edge_index=edge_index)
+
+
+
+
+def build_near_wormholes(table: list[list[str]]):
+    near_wormholes = set()
+    len_row = len(table[0])
+    for i in range(len(table)):
+        for j in range(len_row):
+            if table[i][j] == "*":
+                if i + 1 < len(table):
+                    near_wormholes.add((i+1) * len_row + j)
+                if i - 1 >= 0:
+                    near_wormholes.add((i-1) * len_row + j)
+                if j + 1 < len_row:
+                    near_wormholes.add(i * len_row + j+1)
+                if j - 1 >= 0:
+                    near_wormholes.add(i * len_row + j-1)
+    return list(near_wormholes)
--- worms_env.py
+++ worms_env.py
@@ -1,17 +1,18 @@
 import gymnasium as gym
 from gymnasium import spaces
-from utils_functions import apply_wormholes_policy
+from utils_functions import build_field
 
 class WormsEnv(gym.Env):
     metadata =  {"render_modes": ["human", "rgb_array"], "render_fps": 4}
 
     def __init__(self, env_file: str, render_mode: str = None, enable_wormholes: bool = False, max_val: int = 0):
-        self.field = []
+        table = []
         with open(env_file, "r") as f:
             f.readline()
             self.snakes_lengths = [int(x) for x in f.readline().split()]
             for line in f:
-                self.field.append(apply_wormholes_policy(line.split(), enable_wormholes, max_val))
+                table.append(line.split())
+        self.field = build_field(table)
         self.render_mode = render_mode
         self.window = None
         self.clock = None
